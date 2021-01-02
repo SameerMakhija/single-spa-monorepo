bbaacd0f16ebfeaef39187ac03c74724e6866787
nisheed.jagadish <nisheed.jagadish@loblaw.ca>
2021-01-02 14:32:56 +0530

    Reverted eslint config

--- packages/app-template/.eslintrc.js
+++ packages/app-template/.eslintrc.js
@@ -1,9 +1,22 @@
 module.exports = {
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
     extends: [
         'plugin:react/recommended',
         'airbnb',
         'plugin:prettier/recommended',
     ],
     parser: 'babel-eslint',
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['react'],
     rules: {},
 };
