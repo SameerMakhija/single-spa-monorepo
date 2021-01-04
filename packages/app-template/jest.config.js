817a45e8612664a19fb2dded370783def4e033cc
nisheed.jagadish <nisheed.jagadish@loblaw.ca>
2021-01-04 19:41:43 +0530

    Added test-utils.js to coverage ignore

--- packages/app-template/jest.config.js
+++ packages/app-template/jest.config.js
@@ -14,6 +14,7 @@ module.exports = {
         '/src/server.js',
         '(.scss|.css)$',
         '/components/.*/index.js',
+        '/__tests__/test-utils.js',
     ],
     testPathIgnorePatterns: ['/node_modules/', '__tests__/test-utils.js'],
 };
